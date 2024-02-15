e1fb7b2c572285dccbc820182a0188d5c6c1a63b
zhanghao_home <1981053231@qq.com>
2024-02-15 18:39:15 +0800

    initial

---
+++ src/main/java/com/zh/sillydev/SillydevApplication.java
@@ -0,0 +1,13 @@
+package com.zh.sillydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SillydevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SillydevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zh/sillydev/SillydevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zh.sillydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SillydevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
